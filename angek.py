a45cda41b226d68260caedab93005e07351d92d9
angelaMwende <angelamwnd.am@gmail.com>
2019-10-05 16:31:51 +0300

    my first program

---
+++ angek.py
@@ -0,0 +1,11 @@
+def sum():
+  a=int(input("Enter value a "))
+  b=int(input("enter value b "))
+  total=a+b
+  if total < = 50:
+    return "the sum is more than 50"
+  else:
+    return a+b
+
+
+print(sum())
